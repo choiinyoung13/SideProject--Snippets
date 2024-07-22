4ac6ff8c5316a4ea362c55c966bc01165f0d6ecb
DESKTOP-36S7C96\최지영 <chldls153@gmail.com>
2024-07-22 20:30:21 +0900

    updata: search 기능 제외하고 전부 완성

--- todo/app/_components/Todo.tsx
+++ todo/app/_components/Todo.tsx
@@ -1,15 +1,95 @@
-import { Checkbox, IconButton } from "@material-tailwind/react";
+"use client";
 
-export default function Todo() {
+import { Checkbox, IconButton, Spinner } from "@material-tailwind/react";
+import { useMutation } from "@tanstack/react-query";
+import { deleteTodo, updateTodo } from "app/_actions/todo-actions";
+import { queryClient } from "app/config/ReactQueryClientProvider";
+import { useState } from "react";
+
+export default function Todo({ todo }) {
+  const [isCompleted, setIsCompleted] = useState(todo.completed);
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [title, setTitle] = useState(todo.title);
+
+  const updataTodoMutation = useMutation({
+    mutationFn: () =>
+      updateTodo({
+        title,
+        id: todo.id,
+        completed: isCompleted,
+      }),
+    onSuccess: () => {
+      setIsEditMode(false);
+      queryClient.invalidateQueries({ queryKey: ["todo"] });
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+
+  const deleteTodoMutation = useMutation({
+    mutationFn: () => deleteTodo(todo.id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todo"] });
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+
   return (
-    <div className="flex items-center w-full">
-      <Checkbox />
-      <p className="flex-1">NEW TODO</p>
-      <IconButton>
-        <i className="fas fa-pen" />
-      </IconButton>
-      <IconButton>
-        <i className="fas fa-trash" />
+    <div className="flex items-center w-full gap-1 mt-2">
+      <Checkbox
+        onChange={async () => {
+          await setIsCompleted((prev) => !prev);
+          await updataTodoMutation.mutate();
+        }}
+        checked={isCompleted}
+      />
+      {isEditMode ? (
+        <input
+          className="flex-1 border-b-black border-b p-1"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+      ) : (
+        <p className={`flex-1 ${isCompleted && "line-through"}`}>{title}</p>
+      )}
+
+      {isEditMode ? (
+        <IconButton
+          onClick={async () => {
+            await setIsEditMode((prev) => (prev = false));
+            await updataTodoMutation.mutate();
+          }}
+        >
+          {updataTodoMutation.isPending ? (
+            <Spinner className="h-10 w-10" />
+          ) : (
+            <i className="fas fa-check" />
+          )}
+        </IconButton>
+      ) : (
+        <IconButton
+          onClick={() => {
+            setIsEditMode((prev) => (prev = true));
+          }}
+        >
+          <i className="fas fa-pen" />
+        </IconButton>
+      )}
+
+      <IconButton
+        onClick={() => {
+          deleteTodoMutation.mutate();
+        }}
+      >
+        {deleteTodoMutation.isPending ? (
+          <Spinner className="h-10 w-10" />
+        ) : (
+          <i className="fas fa-trash" />
+        )}
       </IconButton>
     </div>
   );
--- todo/app/_components/ui.tsx
+++ todo/app/_components/ui.tsx
@@ -1,14 +1,58 @@
 "use client";
 
-import { Input } from "@material-tailwind/react";
+import { Button, Input } from "@material-tailwind/react";
 import Todo from "./Todo";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { createTodo, getTodos } from "app/_actions/todo-actions";
+import { useState } from "react";
+import { Spinner } from "@material-tailwind/react";
 
 export default function UI() {
+  const [searchInput, setSearchInput] = useState("");
+
+  const todoQeury = useQuery({
+    queryKey: ["todo"],
+    queryFn: () => getTodos({ searchInput }),
+  });
+
+  const createTodoMutation = useMutation({
+    mutationFn: () =>
+      createTodo({
+        title: "Write your plan",
+        completed: false,
+      }),
+    onSuccess: () => {
+      todoQeury.refetch();
+    },
+    onError: (err) => {
+      console.error(err);
+    },
+  });
+
   return (
     <div className="w-2/3 mx-auto flex flex-col items-center">
       <h1 className="text-xl pt-10 mb-3">TODO LIST</h1>
-      <Input label="Search TODO" icon={<i className="fas fa-search" />} />
-      <Todo />
+      <Input
+        label="Search TODO"
+        icon={<i className="fas fa-search" />}
+        onChange={(e) => {
+          setSearchInput(e.target.value);
+        }}
+      />
+      {todoQeury.isPending && <Spinner className="h-10 w-10 py-8" />}
+      {todoQeury.data &&
+        todoQeury.data.map((todo) => {
+          return <Todo key={todo.id} todo={todo} />;
+        })}
+      <Button
+        onClick={() => {
+          createTodoMutation.mutate();
+        }}
+        loading={createTodoMutation.isPending}
+      >
+        <i className="fas fa-add mr-3" />
+        ADD TODO
+      </Button>
     </div>
   );
 }
