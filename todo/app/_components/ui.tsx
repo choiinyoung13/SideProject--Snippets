37b7e0398db4d151e449a3b4faf6ecd8acf864a1
chldls153@naver.com <chldls153@naver.com>
2024-07-23 13:53:49 +0900

    updata: todo 완성

--- todo/app/_components/ui.tsx
+++ todo/app/_components/ui.tsx
@@ -1,33 +1,33 @@
-"use client";
+'use client'
 
-import { Button, Input } from "@material-tailwind/react";
-import Todo from "./Todo";
-import { useMutation, useQuery } from "@tanstack/react-query";
-import { createTodo, getTodos } from "app/_actions/todo-actions";
-import { useState } from "react";
-import { Spinner } from "@material-tailwind/react";
+import { Button, Input } from '@material-tailwind/react'
+import Todo from './Todo'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { createTodo, getTodos } from 'app/_actions/todo-actions'
+import { useState } from 'react'
+import { Spinner } from '@material-tailwind/react'
 
 export default function UI() {
-  const [searchInput, setSearchInput] = useState("");
+  const [searchInput, setSearchInput] = useState('')
 
   const todoQeury = useQuery({
-    queryKey: ["todo"],
+    queryKey: ['todo'],
     queryFn: () => getTodos({ searchInput }),
-  });
+  })
 
   const createTodoMutation = useMutation({
     mutationFn: () =>
       createTodo({
-        title: "Write your plan",
+        title: 'Write your plan',
         completed: false,
       }),
     onSuccess: () => {
-      todoQeury.refetch();
+      todoQeury.refetch()
     },
-    onError: (err) => {
-      console.error(err);
+    onError: err => {
+      console.error(err)
     },
-  });
+  })
 
   return (
     <div className="w-2/3 mx-auto flex flex-col items-center">
@@ -35,24 +35,26 @@ export default function UI() {
       <Input
         label="Search TODO"
         icon={<i className="fas fa-search" />}
-        onChange={(e) => {
-          setSearchInput(e.target.value);
+        onChange={async e => {
+          await setSearchInput(e.target.value)
+          await todoQeury.refetch()
         }}
       />
-      {todoQeury.isPending && <Spinner className="h-10 w-10 py-8" />}
+      {todoQeury.isLoading && <Spinner className="h-10 w-10 my-8" />}
       {todoQeury.data &&
-        todoQeury.data.map((todo) => {
-          return <Todo key={todo.id} todo={todo} />;
+        todoQeury.data.map(todo => {
+          return <Todo key={todo.id} todo={todo} />
         })}
       <Button
         onClick={() => {
-          createTodoMutation.mutate();
+          createTodoMutation.mutate()
         }}
+        className="mt-4"
         loading={createTodoMutation.isPending}
       >
-        <i className="fas fa-add mr-3" />
+        <i className="fas fa-add mr-5" />
         ADD TODO
       </Button>
     </div>
-  );
+  )
 }
