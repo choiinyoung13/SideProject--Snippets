2b9aced3c3e75dfa7c6b4d2444ab66d4f8780e42
DESKTOP-36S7C96\최지영 <chldls153@gmail.com>
2024-07-24 15:21:39 +0900

    updata: dropbox drag drop으로 파일업로드하는 기능 구현

--- dropbox/app/_components/DragDropSection.tsx
+++ dropbox/app/_components/DragDropSection.tsx
@@ -1,43 +1,48 @@
-'use client'
+"use client";
 
-import { Button } from '@material-tailwind/react'
-import { useMutation } from '@tanstack/react-query'
-import { uploadFile } from 'app/_actions/storageActions'
-import { queryClient } from 'app/_config/ReactQueryClientProvider'
-import { useRef } from 'react'
+import { Button, Spinner } from "@material-tailwind/react";
+import { useMutation } from "@tanstack/react-query";
+import { uploadFile } from "app/_actions/storageActions";
+import { queryClient } from "app/_config/ReactQueryClientProvider";
+import { useCallback, useRef } from "react";
+import { useDropzone } from "react-dropzone";
 
 export default function DragDropSection() {
-  const inputRef = useRef(null)
+  const inputRef = useRef(null);
   const uploadImageMutation = useMutation({
     mutationFn: uploadFile,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['images'] })
+      queryClient.invalidateQueries({ queryKey: ["images"] });
     },
-  })
+  });
 
-  const onSubmit = e => {
-    e.preventDefault()
-    const file = inputRef.current.files?.[0]
+  const onDrop = useCallback((acceptedFiles) => {
+    // inputRef.current.files  === acceptedFiles
+    // 원래: const file = inputRef.current.files?.[0];
+    const file = acceptedFiles?.[0];
 
     if (file) {
-      const formData = new FormData()
-      formData.append('file', file)
-      uploadImageMutation.mutate(formData)
+      const formData = new FormData();
+      formData.append("file", file);
+      uploadImageMutation.mutate(formData);
     }
-  }
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <form
+    <div
       className="w-full h-[200px] border-dotted border-indigo-700 border-4 flex flex-col items-center justify-center gap-3"
-      onSubmit={onSubmit}
+      {...getRootProps()}
     >
-      <input ref={inputRef} type="file" />
-      <p className="font-bold">
-        파일을 여기다 끌어다 놓거나 클릭하여 업로드하세요.
-      </p>
-      <Button type="submit" loading={uploadImageMutation.isPending}>
-        파일 업로드
-      </Button>
-    </form>
-  )
+      <input {...getInputProps()} />
+      {uploadImageMutation.isPending ? (
+        <Spinner />
+      ) : isDragActive ? (
+        <p>파일을 놓아주세요.</p>
+      ) : (
+        <p>파일을 여기다 끌어다 놓거나 클릭하여 업로드하세요.</p>
+      )}
+    </div>
+  );
 }
